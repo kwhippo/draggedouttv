b1cbfd0feee3510e8859af6275cc1def4407107e
kwhippo <kevin@aceauctioncompany.com>
2016-01-28 21:44:46 -0600

    create blog url patterns

--- draggedouttv/urls.py
+++ draggedouttv/urls.py
@@ -18,4 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^blog/', include('dragblog.urls', namespace='blog', app_name='dragblog')),
 ]
