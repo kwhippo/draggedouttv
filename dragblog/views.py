9bdad8feaeec76e0c4b55015afcf479b2349d70e
kwhippo <kevin@aceauctioncompany.com>
2016-02-08 20:23:41 -0600

    add tagging functionality to posts and views

--- dragblog/views.py
+++ dragblog/views.py
@@ -1,25 +1,32 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
+from taggit.models import Tag
 
 from .forms import CommentForm
 from .models import Post
 
 
-# def post_list(request):
-#     object_list = Post.published.all()
-#     paginator = Paginator(object_list, 3)
-#     page = request.GET.get('page')
-#     try:
-#         posts = paginator.page(page)
-#     except PageNotAnInteger:
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         posts = paginator.page(paginator.num_pages)
-#     return render(
-#             request,
-#             'blog/post/list.html',
-#             {'page': page, 'posts': posts}
-#     )
+def post_list(request, tag_slug=None):
+    object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(
+            request,
+            'blog/post/list.html',
+            {'page': page, 'posts': posts, 'tag': tag}
+    )
 
 
 def post_detail(request, year, month, day, post):
