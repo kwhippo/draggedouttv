d358cee5ea65a26e1ef92f47d2778041245543a8
kwhippo <kevin@aceauctioncompany.com>
2016-02-08 19:39:54 -0600

    add comment to admin

--- dragblog/admin.py
+++ dragblog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Post, Comment
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -13,4 +13,11 @@ class PostAdmin(admin.ModelAdmin):
     ordering = ['status', 'publish']
 
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
+
+
+admin.site.register(Comment)
 admin.site.register(Post)
