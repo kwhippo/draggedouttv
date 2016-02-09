a41cbde6e77e8b1b0d92cb45de065d4aea05708e
kwhippo <kevin@aceauctioncompany.com>
2016-02-08 19:56:58 -0600

    add comment form and comments to post detail view

--- dragblog/views.py
+++ dragblog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 
+from .forms import CommentForm
 from .models import Post
 
 
@@ -24,7 +25,16 @@ from .models import Post
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month,
                              publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+    comments = post.comments.filter(active=True)
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'comment_form': comment_form})
 
 
 class PostListView(ListView):
