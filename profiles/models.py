866d0bbf7aa5c7b4d56d7d9139db1d0ca0b612ec
kwhippo <kevin@aceauctioncompany.com>
2016-03-30 23:07:22 -0500

    add basic drag profile model fields

--- profiles/models.py
+++ profiles/models.py
@@ -1,6 +1,12 @@
 from django.contrib.auth.models import User
 from django.db import models
+from taggit.managers import TaggableManager
 
 
 class Profile(models.Model):
-    author = models.ForeignKey(User, related_name='profile')
+    author = models.OneToOneField(User)
+    drag_name = models.CharField(blank=False, null=False, max_length=50)
+    given_name = models.CharField(blank=True, null=True, max_length=50)
+    name_origin = models.TextField(blank=True, null=True, max_length=150)
+    likes = TaggableManager()
+    about = models.TextField(blank=True, null=True, max_length=250)
